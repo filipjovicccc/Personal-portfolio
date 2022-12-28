974d35ae9a2623e433f6d45ed85ba5e916a9848a
filipjovicccc <filipjovicccc@gmail.com>
2022-12-28 19:44:55 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+import Button from './Button'
+
+function Header() {
+  return (
+    <div className='header'>
+        <h2>Personal portfolio</h2>
+        <Button title="Call us"/>
+       
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Main.css"
+import image1 from "../assets/images/heading.avif"
+
+function Main() {
+  return (
+    <div>
+  <img className='image' src={image1} alt="heading"></img>
+    </div>
+  )
+}
+
+export default Main
