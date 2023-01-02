19368de2b93e2473483be7d0268f3495fe995ebb
filipjovicccc <filipjovicccc@gmail.com>
2023-01-02 14:17:49 +0100

    working on home page

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,16 @@ import image1 from "../assets/images/heading.avif"
 function Main() {
   return (
     <div>
-  <img className='image' src={image1} alt="heading"></img>
+        <div className='image-section'>
+      
+          <img className='image' src={image1} alt="heading"></img>
+         
+
+         <section className='main-section'>
+             <h1>Filip Jovic Jagodina Web Development Success</h1>
+             <p>Hello i am Filip jovic, fronEnd developer i can help district businesses create professional, modern and user-friendly web sites. With our reliable developers, we ensure quality and innovative designs from the start to provide customers with an effective online presence.</p>
+         </section>
+         </div>
     </div>
   )
 }
